fa4a4546f5fc7bd4e19dc9c61dbb3c3b314156f8
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-13 21:57:08 -0800

    Initial commit

---
+++ JS320-final/src/Header.jsx
@@ -0,0 +1,12 @@
+import title from './images/title.jpg'
+
+function Header() {
+
+    return (
+    <header>
+        <img src={title} alt='The words "Black Sails" ' className='title'/>
+    </header>
+    )
+  }
+  
+  export default Header
