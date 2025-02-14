cdfb338066ef4501b8e9bc57f4a29d53fadb250f
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-13 19:45:02 -0800

    Initial commit

---
+++ JS320-final/src/Homepage.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom'
+
+function Homepage() {
+    const navigate = useNavigate()
+  
+  
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Homepage
