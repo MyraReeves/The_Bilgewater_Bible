53e0f6a73cc65409348647bc96a324809c844600
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-16 02:16:52 -0800

    Adds route to stats page

--- JS320-final/src/RoutePaths.jsx
+++ JS320-final/src/RoutePaths.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 
 import Homepage from './Homepage'
 import CharacterIndex from './CharacterList'
+import TVMazeAPI from './TVMazeAPI'
 
 function RoutePaths() {
 
@@ -11,13 +12,18 @@ function RoutePaths() {
 
     const Characters = () => {
         return <CharacterIndex/>
-      }
+    }
+
+    const Stats = () => {
+        return <TVMazeAPI/>
+    }
 
 
     return (
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/characters' element={<Characters />}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/characters' element={<Characters />} />
+          <Route path='/stats' element={<Stats />} />
         </Routes>
     )
 }
