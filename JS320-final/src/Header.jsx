af1be4e3f6139290f73b87a19d255d4bb64abee3
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-16 23:59:29 -0800

    Adds dropdown menu

--- JS320-final/src/Header.jsx
+++ JS320-final/src/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import title from './images/title.jpg'
 
 function Header() {
@@ -5,6 +6,14 @@ function Header() {
     return (
     <header>
         <img src={title} alt='The words "Black Sails" ' className='title'/>
+        <div className='dropdown-menu'>
+            <button className='dropdown-button'>Menu</button>
+            <ul className='dropdown-content'>
+                <li><Link to='/'> 🏠︎ Homepage </Link></li>
+                <li><Link to='/cast'> Cast </Link></li>
+                <li><Link to='/characters'> Characters </Link></li>
+            </ul>
+        </div>
     </header>
     )
   }
