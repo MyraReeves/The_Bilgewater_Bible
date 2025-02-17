78ebdfd1041b514f6a503a32a390a5729e7a72e8
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-17 00:54:36 -0800

    Edits contents of drop-down menu

--- JS320-final/src/Header.jsx
+++ JS320-final/src/Header.jsx
@@ -7,11 +7,13 @@ function Header() {
     <header>
         <img src={title} alt='The words "Black Sails" ' className='title'/>
         <div className='dropdown-menu'>
-            <button className='dropdown-button'>Menu</button>
+            <button className='dropdown-button'>🧭 Menu ▾</button>
             <ul className='dropdown-content'>
-                <li><Link to='/'> 🏠︎ Homepage </Link></li>
-                <li><Link to='/cast'> Cast </Link></li>
-                <li><Link to='/characters'> Characters </Link></li>
+                <li><Link to='/'> ⚓ Homepage </Link></li>
+                <li><Link to='/stats'>📜 Basic Facts </Link></li>
+                <li><Link to='/cast'>🎭 Cast </Link></li>
+                <li><Link to='/characters'>🎭 Characters </Link></li>
+                <li><Link to='/shopping'>💰 Shopping </Link></li>
             </ul>
         </div>
     </header>
