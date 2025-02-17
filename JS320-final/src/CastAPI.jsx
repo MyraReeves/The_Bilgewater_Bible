db8a35c28f9b9bd613085746c540c3a99fdff57b
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-16 19:49:50 -0800

    Initial commit

---
+++ JS320-final/src/CastAPI.jsx
@@ -0,0 +1,72 @@
+// This file maps an array of objects given by the API, to generate
+
+import { useEffect, useState } from "react"
+
+function CastAPI() {
+    const [arrayOfcastMembers, setArrayOfcastMembers] = useState([]);
+    const [hasError, setHasError] = useState(false);
+
+    useEffect( () => {
+        fetch('https://api.tvmaze.com/shows/182/cast')
+
+            // Uncomment this block of code if needed for checking the response sent by the API:
+            /*
+             .then( response => {
+                if(response.status == 200){
+                    const additionalInfo = '-- Fetch of requested data succeeded! 🎉';
+                    console.log('The response status from the API was:', response.status, additionalInfo)
+                };
+                if(response.status == 429){
+                    const additionalInfo = "-- Your IP address has sent too many requests within a given amount of time. It exceeded the API's rate limits. 🏇💨"
+                    console.log('The response status from the API was:', response.status, additionalInfo)
+                };
+                if(response.status != 200 && response.status != 429) {
+                    console.log('The response status from the API was:', response.status)
+                };
+             })
+            */
+
+            .then( response => response.json() )
+
+            .then( objectArrayInsideResponse => {
+                setArrayOfcastMembers(objectArrayInsideResponse);
+            })
+
+            .catch( error => {
+                console.log('%cThe following error occured when attempting to fetch data from the API: \n', 'color: red; font-weight: bold; font-size: larger', error);
+                setHasError(true);
+            });
+        }, []);
+
+
+    if (hasError) {
+        return <div className="error">⛔ An error occurred while fetching the information.  Sorry! ⛔ <br/>Please check the console for further details.</div>;
+    }
+
+    if (arrayOfcastMembers === null) {
+        return <div className="loading">Loading information...</div>;
+    }
+
+    // arrayOfcastMembers.sort();
+
+  return (
+    <main>
+        <h1>☠️ <span className="underline">Cast Members</span> ☠️</h1>
+        {arrayOfcastMembers.map( (eachCastMember, index) => (
+            <div key={index} className="cast-list">
+                <h2> {eachCastMember.person.name} </h2>
+                <img src = {eachCastMember.person.image.medium} alt = 'Photo of the actor'/>
+                <p> Nationality: &nbsp; <span className="info"> {eachCastMember.person.country.name} </span> </p>
+                <p> Born on: &nbsp; <span className="info"> {eachCastMember.person.birthday} </span> </p>
+                <p> Portrays: &nbsp; <span className="info"> {eachCastMember.character.name} </span> </p>
+            </div>
+            ))
+        }
+
+
+    </main>
+  )
+}
+
+
+export default CastAPI
