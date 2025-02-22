e8a9578d64405ea9f315b121f3e73d44df9d6b4d
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-22 00:20:09 -0800

    Adds Rotten Tomatoes ratings

--- JS320-final/src/Homepage.jsx
+++ JS320-final/src/Homepage.jsx
@@ -19,7 +19,11 @@ function Homepage() {
                     Released from January-March 2014
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/rT2Y5jjBNpQ?si=e50p1R5prTEpyVgE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>Average User Rating on IMDB: 8 out of 10 stars</p>
+                <p className='IMDB-rating'>
+                    Average User Rating on IMDB: 8 out of 10 stars <br/>
+                    Audience Rating on Rotten Tomatoes: 79% <br/>
+                    Critics Reviews: 65%
+                </p>
             </div>
 
             <div className='trailers'>
@@ -29,7 +33,11 @@ function Homepage() {
                     Released from January-March 2015
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/OvaXhRq8VB4?si=xWnmZq23ReBcpdu0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>Average User Rating on IMDB: 8.5 out of 10 stars</p>
+                <p className='IMDB-rating'>
+                    Average User Rating on IMDB: 8.5 out of 10 stars <br/>
+                    Audience Rating on Rotten Tomatoes: 90% <br/>
+                    Critics Reviews: 100%
+                </p>
             </div>
 
             <div className='trailers'>
@@ -39,7 +47,11 @@ function Homepage() {
                     Released from January-March 2016
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/Uq61obO9lww?si=C5brh_Bn7-NAhkyh" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>Average User Rating on IMDB: 9 out of 10 stars</p>
+                <p className='IMDB-rating'>
+                    Average User Rating on IMDB: 9 out of 10 stars <br/>
+                    Audience Rating on Rotten Tomatoes: 86% <br/> 
+                    Critics Reviews: 80%
+                </p>
             </div>
 
             <div className='trailers'>
@@ -49,7 +61,11 @@ function Homepage() {
                     Released from January-April 2017
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/gyJW2Xrf6EI?si=1x5dnwZo8BzjQ1Ap" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>Average User Rating on IMDB: 8.5 out of 10 stars</p>
+                <p className='IMDB-rating'>
+                    Average User Rating on IMDB: 8.5 out of 10 stars <br/>
+                    Audience Rating on Rotten Tomatoes: 78% <br/>
+                    Critics Reviews: 80%
+                </p>
             </div>
 
             <p className='watch-on'>Watch all 4 seasons on: <br/>   
