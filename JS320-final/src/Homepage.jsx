03daa84dc170e5d46044705520aa9cc4a13ad6e7
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-22 21:11:31 -0800

    Removes ratings and adds opening theme song renditions

--- JS320-final/src/Homepage.jsx
+++ JS320-final/src/Homepage.jsx
@@ -19,11 +19,6 @@ function Homepage() {
                     Released from January-March 2014
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/rT2Y5jjBNpQ?si=e50p1R5prTEpyVgE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>
-                    Average User Rating on IMDB: 8 out of 10 stars <br/>
-                    Audience Rating on Rotten Tomatoes: 79% <br/>
-                    Critics Reviews: 65%
-                </p>
             </div>
 
             <div className='trailers'>
@@ -33,11 +28,6 @@ function Homepage() {
                     Released from January-March 2015
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/OvaXhRq8VB4?si=xWnmZq23ReBcpdu0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>
-                    Average User Rating on IMDB: 8.5 out of 10 stars <br/>
-                    Audience Rating on Rotten Tomatoes: 90% <br/>
-                    Critics Reviews: 100%
-                </p>
             </div>
 
             <div className='trailers'>
@@ -47,11 +37,6 @@ function Homepage() {
                     Released from January-March 2016
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/Uq61obO9lww?si=C5brh_Bn7-NAhkyh" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>
-                    Average User Rating on IMDB: 9 out of 10 stars <br/>
-                    Audience Rating on Rotten Tomatoes: 86% <br/> 
-                    Critics Reviews: 80%
-                </p>
             </div>
 
             <div className='trailers'>
@@ -61,11 +46,6 @@ function Homepage() {
                     Released from January-April 2017
                 </p>
                 <iframe width="660" height="315" src="https://www.youtube.com/embed/gyJW2Xrf6EI?si=1x5dnwZo8BzjQ1Ap" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                <p className='IMDB-rating'>
-                    Average User Rating on IMDB: 8.5 out of 10 stars <br/>
-                    Audience Rating on Rotten Tomatoes: 78% <br/>
-                    Critics Reviews: 80%
-                </p>
             </div>
 
             <p className='watch-on'>Watch all 4 seasons on: <br/>   
@@ -74,6 +54,17 @@ function Homepage() {
                 <Link to='https://tv.apple.com/us/show/black-sails/umc.cmc.2aq6e38r265c2z6nkjdbacqvd'> <img src={AppleTvLogo} alt='Apple TV +'/> </Link> &nbsp;
                 <Link to='https://www.starz.com/us/en/series/black-sails/season-1/19965'> <img src={StarzLogo} alt='Starz'/> </Link>
             </p>
+
+            <div className='opening-titles'>
+                <div>
+                    <iframe width="660" height="365" src="https://www.youtube.com/embed/PBmzzrU3gR8" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                </div>
+
+                <div style={{marginTop: "50px"}}>
+                    <iframe width="660" height="365" src="https://www.youtube.com/embed/XFTcA4QLHw0" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                </div>
+           </div>
+
         </main>
     )
 }
