3e9cbea530eb04b9a9264d4b1bfd9c2fef77b93e
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-25 01:35:10 -0800

    Initial commit

---
+++ JS320-final/src/Ratings.jsx
@@ -0,0 +1,91 @@
+
+function Ratings() {
+
+  return (
+    <div className="ratings">
+        <h2>★ Ratings ★</h2>
+
+        <table>
+            <thead>
+                Season 1
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Average of User Ratings on IMDB:</td>
+                    <td><span className="rating">8</span></td>
+                </tr>
+                <tr>
+                    <td>Audience Rating on Rotten Tomatoes:</td>
+                    <td><span className="rating">79%</span></td>
+                </tr>
+                <tr>
+                    <td>Critics Reviews on Rotten Tomatoes:</td>
+                    <td><span className="rating">65%</span></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table>
+            <thead>
+                Season 2
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Average of User Ratings on IMDB:</td>
+                    <td><span className="rating">8.5</span></td>
+                </tr>
+                <tr>
+                    <td>Audience Rating on Rotten Tomatoes:</td>
+                    <td><span className="rating">90%</span></td>
+                </tr>
+                <tr>
+                    <td>Critics Reviews on Rotten Tomatoes:</td>
+                    <td><span className="rating">100%</span></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table>
+            <thead>
+                Season 3
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Average of User Ratings on IMDB:</td>
+                    <td><span className="rating">9</span></td>
+                </tr>
+                <tr>
+                    <td>Audience Rating on Rotten Tomatoes:</td>
+                    <td><span className="rating">86%</span></td>
+                </tr>
+                <tr>
+                    <td>Critics Reviews on Rotten Tomatoes:</td>
+                    <td><span className="rating">80%</span></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table>
+            <thead>
+                Season 4
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Average of User Ratings on IMDB:</td>
+                    <td><span className="rating">8.5</span></td>
+                </tr>
+                <tr>
+                    <td>Audience Rating on Rotten Tomatoes:</td>
+                    <td><span className="rating">78%</span></td>
+                </tr>
+                <tr>
+                    <td>Critics Reviews on Rotten Tomatoes:</td>
+                    <td><span className="rating">80%</span></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Ratings
