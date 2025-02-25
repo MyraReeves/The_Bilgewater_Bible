e81fde4ed49e6d63bf7264572911b084c32eb423
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-25 01:34:52 -0800

    Moves ratings into a separate file that is imported here instead

--- JS320-final/src/TVMazeAPI.jsx
+++ JS320-final/src/TVMazeAPI.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
+import Ratings from "./Ratings";
 
 function TVMazeAPI() {
     const [object, setObject] = useState(null);
@@ -51,89 +52,8 @@ function TVMazeAPI() {
                 </p>
             </div>
 
-            <div className="ratings">
-                <h2>★ Ratings ★</h2>
 
-                <table>
-                    <thead>
-                        Season 1
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Average of User Ratings on IMDB:</td>
-                            <td><span className="rating">8</span></td>
-                        </tr>
-                        <tr>
-                            <td>Audience Rating on Rotten Tomatoes:</td>
-                            <td><span className="rating">79%</span></td>
-                        </tr>
-                        <tr>
-                            <td>Critics Reviews on Rotten Tomatoes:</td>
-                            <td><span className="rating">65%</span></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <table>
-                    <thead>
-                        Season 2
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Average of User Ratings on IMDB:</td>
-                            <td><span className="rating">8.5</span></td>
-                        </tr>
-                        <tr>
-                            <td>Audience Rating on Rotten Tomatoes:</td>
-                            <td><span className="rating">90%</span></td>
-                        </tr>
-                        <tr>
-                            <td>Critics Reviews on Rotten Tomatoes:</td>
-                            <td><span className="rating">100%</span></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <table>
-                    <thead>
-                        Season 3
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Average of User Ratings on IMDB:</td>
-                            <td><span className="rating">9</span></td>
-                        </tr>
-                        <tr>
-                            <td>Audience Rating on Rotten Tomatoes:</td>
-                            <td><span className="rating">86%</span></td>
-                        </tr>
-                        <tr>
-                            <td>Critics Reviews on Rotten Tomatoes:</td>
-                            <td><span className="rating">80%</span></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <table>
-                    <thead>
-                        Season 4
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Average of User Ratings on IMDB:</td>
-                            <td><span className="rating">8.5</span></td>
-                        </tr>
-                        <tr>
-                            <td>Audience Rating on Rotten Tomatoes:</td>
-                            <td><span className="rating">78%</span></td>
-                        </tr>
-                        <tr>
-                            <td>Critics Reviews on Rotten Tomatoes:</td>
-                            <td><span className="rating">80%</span></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <Ratings />
 
 
             <img src={object.image.medium} alt='Season 4 official teaser poster for Black Sails' />
