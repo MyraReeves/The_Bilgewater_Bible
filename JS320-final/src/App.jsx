f201da902cf467def19046a72261b05a1c9696d8
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-27 19:52:31 -0800

    Defines props used for API errors

--- JS320-final/src/App.jsx
+++ JS320-final/src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import RoutePaths from './RoutePaths';
 import Header from './Header'
 
 
 function App() {
+  const [hasError, setHasError] = useState(false);
 
   return (
     <>
       <Header />
-      <RoutePaths />
+      <RoutePaths 
+        hasError={hasError} 
+        setHasError={setHasError}
+      />
     </>
   )
 }
