7d17b44fbf2247dee9b4b640c5097690f6d80189
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-27 23:27:38 -0800

    Simplifies iframe code

--- JS320-final/src/Homepage.jsx
+++ JS320-final/src/Homepage.jsx
@@ -7,19 +7,19 @@ import AppleTvLogo from './images/AppleTv.png';
 import StarzLogo from './images/starz.png';
 
 function Homepage() {
-      
+    
     return (
         <main>
             <PirateAPI/>
             <EpisodeList/>
-
+            
             <div className='trailers'>
                 <p className='homepagePreviews'>Season 1</p>
                 <p className='release-dates'>
                     Episodes: I - VIII<br/>
                     Released from January-March 2014
                 </p>
-                <iframe width="660" height="315" src="https://www.youtube.com/embed/rT2Y5jjBNpQ?si=e50p1R5prTEpyVgE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe width="660" height="315" src="https://www.youtube.com/embed/rT2Y5jjBNpQ?rel=0" frameBorder="0" allowFullScreen title="The first season's official preview trailer. This video is hosted on YouTube" />
             </div>
 
             <div className='trailers'>
@@ -28,7 +28,7 @@ function Homepage() {
                     Episodes: IX - XVIII <br/>
                     Released from January-March 2015
                 </p>
-                <iframe width="660" height="315" src="https://www.youtube.com/embed/OvaXhRq8VB4?si=xWnmZq23ReBcpdu0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe width="660" height="315" src="https://www.youtube.com/embed/OvaXhRq8VB4?rel=0" frameBorder="0" allowFullScreen title="The second season's official preview trailer. This video is hosted on YouTube" />
             </div>
 
             <div className='trailers'>
@@ -37,7 +37,7 @@ function Homepage() {
                     Episodes: XIX - XXVIII <br/>
                     Released from January-March 2016
                 </p>
-                <iframe width="660" height="315" src="https://www.youtube.com/embed/Uq61obO9lww?si=C5brh_Bn7-NAhkyh" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe width="660" height="315" src="https://www.youtube.com/embed/Uq61obO9lww?rel=0" frameBorder="0" allowFullScreen title="The third season's official preview trailer. This video is hosted on YouTube" />
             </div>
 
             <div className='trailers'>
@@ -46,7 +46,7 @@ function Homepage() {
                     Episodes: XXIX - XXXVIII <br/>
                     Released from January-April 2017
                 </p>
-                <iframe width="660" height="315" src="https://www.youtube.com/embed/gyJW2Xrf6EI?si=1x5dnwZo8BzjQ1Ap" title="YouTube video player" frameBorder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe width="660" height="315" src="https://www.youtube.com/embed/gyJW2Xrf6EI?rel=0" frameBorder="0" allowFullScreen title="The first season's official preview trailer. This video is hosted on YouTube" />
             </div>
 
             <p className='watch-on'>Watch all 4 seasons on: <br/>   
@@ -58,11 +58,11 @@ function Homepage() {
 
             <div className='opening-titles'>
                 <div>
-                    <iframe width="660" height="365" src="https://www.youtube.com/embed/PBmzzrU3gR8" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="660" height="365" src="https://www.youtube.com/embed/PBmzzrU3gR8?rel=0" allowFullScreen title="A video of the musicians playing the opening credits theme song. This video is hosted on YouTube" />
                 </div>
 
                 <div style={{marginTop: "50px"}}>
-                    <iframe width="660" height="365" src="https://www.youtube.com/embed/XFTcA4QLHw0" title="YouTube video player"  frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="660" height="365" src="https://www.youtube.com/embed/XFTcA4QLHw0?rel=0" frameBorder="0" allowFullScreen title="The show's opening credits. This video is hosted on YouTube" />
                 </div>
            </div>
 
