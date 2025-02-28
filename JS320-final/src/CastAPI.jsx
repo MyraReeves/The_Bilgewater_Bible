347bc38932290f9ee41ae77a688f0f252eab1e1c
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-27 20:36:58 -0800

    Changes hasError variables into props that the function inherits

--- JS320-final/src/CastAPI.jsx
+++ JS320-final/src/CastAPI.jsx
@@ -1,31 +1,14 @@
 // This file maps an array of objects given by the API, in order to generate a visual list on the screen of each actor's photo, name, birthdate, and who they portrayed on the show.
 
-
 import { useEffect, useState } from "react"
+import PropTypes from "prop-types";
 
-function CastAPI() {
+function CastAPI(props) {
     const [arrayOfcastMembers, setArrayOfcastMembers] = useState([]);
-    const [hasError, setHasError] = useState(false);
+    const {hasError, setHasError} = props;
 
     useEffect( () => {
         fetch('https://api.tvmaze.com/shows/182/cast')
-
-            // Uncomment this block of code if needed for checking the response sent by the API:
-            /*
-             .then( response => {
-                if(response.status == 200){
-                    const additionalInfo = '-- Fetch of requested data succeeded! 🎉';
-                    console.log('The response status from the API was:', response.status, additionalInfo)
-                };
-                if(response.status == 429){
-                    const additionalInfo = "-- Your IP address has sent too many requests within a given amount of time. It exceeded the API's rate limits. 🏇💨"
-                    console.log('The response status from the API was:', response.status, additionalInfo)
-                };
-                if(response.status != 200 && response.status != 429) {
-                    console.log('The response status from the API was:', response.status)
-                };
-             })
-            */
 
             .then( response => response.json() )
 
@@ -49,7 +32,7 @@ function CastAPI() {
     }
 
 
-    // Sort the array in alphabetical order of the actor names:
+    // Sort the array in alphabetical order based on first names:
     arrayOfcastMembers.sort(
         function(object1, object2) {
             if (object1.person.name < object2.person.name) {
@@ -87,5 +70,9 @@ function CastAPI() {
   )
 }
 
+CastAPI.propTypes = {
+    hasError: PropTypes.bool.isRequired,
+    setHasError: PropTypes.func.isRequired,
+}
 
 export default CastAPI
