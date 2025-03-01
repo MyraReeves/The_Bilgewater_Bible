ff8e1c2670b2c9bd410e6a5dbed3038daab2f594
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-01 02:20:13 -0800

    Updates the text returned

--- JS320-final/src/PirateAPI.jsx
+++ JS320-final/src/PirateAPI.jsx
@@ -1,30 +1,43 @@
-import { useEffect, useState } from "react"
+
+// import { useEffect, useState } from "react"
 
 
 function PirateAPI() {
-    const [pirateGreeting, setPirateGreeting] = useState(undefined)
-    const [hasError, setHasError] = useState(false)
+    // const [pirateGreeting, setPirateGreeting] = useState(undefined)
+    // const [hasError, setHasError] = useState(false)
 
-    useEffect( () => {
-        fetch('https://pirate.monkeyness.com/api/insult')
-          .then( response => response.json() )
-          .then( string => {
-            setPirateGreeting(string);
-            console.log({pirateGreeting})
-          }, error => {console.log(error)} )
-          .catch( () => {
-            setHasError(true);
-          } )
-    }, [] )
+    // useEffect( () => {
+    //     fetch('https://api.tvmaze.com/shows/182')
+    //       .then( response => response.json() )
+    //       .then( string => {
+    //         setPirateGreeting(string);
+    //         console.log(string);
+    //         console.log({pirateGreeting});
+    //         const object = string;
+    //         console.log(object)
+    //         console.log(object.language)
+    //         return object;
+    //       }, error => {console.log(error)} )
+    //       .catch( () => {
+    //         setHasError(true);
+    //       } )
+    //       .finally( () => {
+    //         return pirateGreeting
+    //       }
+    //       )
+    // }, [] )
 
-    if (hasError) {
-        return ' '
-    }
+    // console.log(pirateGreeting.ended)
+
+    // if (hasError) {
+    //     return ' '
+    // }
 
   return (
-    <>
-        <h2 className="greeting">Welcome! {pirateGreeting}</h2>
-    </>
+    <div className="greeting">
+        <h2>Welcome to Governor Augustus Featherstone's Complete and Accurate, Totally Fictional <br/>Journal of Who's Who in Nassau Town Circa 1715.</h2> 
+        <p>Here-in lies an account of flibustiers, buccaneers, freebooters, maroons, privateers, and the occassional jungaless. <br/> Dis ting go with sense and keep yer wits about ye. For there is no peace beyond the line!</p>
+    </div>
   )
 }
 
