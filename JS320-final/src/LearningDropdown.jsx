e6c13dbf2a7017b3b65c7ffa746094e20ec1c5ad
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-28 21:16:02 -0800

    Adds functionality to the dropdown menu

--- JS320-final/src/LearningDropdown.jsx
+++ JS320-final/src/LearningDropdown.jsx
@@ -1,28 +1,37 @@
+import PropTypes from "prop-types";
 
+function LearningDropdownMenu(props) {
+    const {selection, setSelection} = props;
 
-function LearningDropdownMenu() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(selection)
+     };
 
-    const selectedOption = (event) => {
-        event.preventDefault()
-
-
+    const handleSelection = (event) => {
+        setSelection(event.target.value);
     }
 
   return (
     <div className="learning-dropdown">
-        <form onSubmit={selectedOption}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="learning-choices">Choose a topic: </label>
-            <select name="learningLibrary" id="learning-choices">
-                <option value="blank"> </option>
+            <select name="learningLibrary" id="learning-choices" onChange={handleSelection} value={selection}>
+                <option disabled> </option>
                 <option value="bahamas">Bahamas</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
                 <option value="option4">Option 4</option>
         </select>
-        <button type="submit">Submit</button>
+        <input type="submit" value="Submit"/>
         </form>
     </div>
   )
+}
+
+LearningDropdownMenu.propTypes = {
+    selection: PropTypes.string.isRequired,
+    setSelection: PropTypes.func.isRequired,
 }
 
 export default LearningDropdownMenu
