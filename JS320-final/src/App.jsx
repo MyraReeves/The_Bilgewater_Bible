7487834f6f98064b3cb9d3b9e6d743b9af417a6b
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-01 23:02:09 -0800

    Imports footer onto page

--- JS320-final/src/App.jsx
+++ JS320-final/src/App.jsx
@@ -2,19 +2,22 @@ import { useState } from 'react';
 import './App.css';
 import RoutePaths from './RoutePaths';
 import Header from './Header'
+import Footer from './Footer'
 
 
 function App() {
   const [hasError, setHasError] = useState(false);
 
   return (
-    <>
+    <body>
       <Header />
       <RoutePaths 
         hasError={hasError} 
         setHasError={setHasError}
       />
-    </>
+      <Footer />
+      
+    </body>
   )
 }
 
