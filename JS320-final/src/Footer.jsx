7b48f00ebc7a30a7efaecee211cdb68d438350dd
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-01 22:47:04 -0800

    Adds a footer to the website

---
+++ JS320-final/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+
+    return (
+        <footer>
+            <hr/>
+            <p>Governor Augustus Featherstone's Complete and Accurate, Totally Fictional Journal of Who's Who in Nassau Town Circa 1715</p>
+            <p>Thank you to the following free APIs:</p>
+        </footer>
+    )
+}
+
+export default Footer
