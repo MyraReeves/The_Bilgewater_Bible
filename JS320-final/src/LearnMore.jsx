d5908422043c9b5e0ab90b9a95b1ba1678d80b1d
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-03 03:07:09 -0800

    Adds conditionals for newly added books and API references for each

--- JS320-final/src/LearnMore.jsx
+++ JS320-final/src/LearnMore.jsx
@@ -1,10 +1,10 @@
 // This file displays onto the screen the LearningDropdown file's menu of options followed by the results of whichever option the user selects.
 
 import { useState } from "react"
-
 import ships from './images/ships1.jpg'
 import LearningDropdownMenu from "./LearningDropdown"
 import BahamasInfo from "./Bahamas"
+import LibraryOfCongressAPI from "./LOC"
 
 function LearnMore() {
     const [selection, setSelection] = useState('')
@@ -18,10 +18,31 @@ function LearnMore() {
             return <div className="box-shadow"> <BahamasInfo/> </div>
         }
 
-        // 1839 book:  "Lives and voyages of Drake, Cavendish, and Dampier"
-        if (selection == 'pirateLives'){
-            return <div className="box-shadow"> LOCAPI </div>
+        // 1725 book: "The history and lives of all the most notorious pirates, and their crews, from Capt. Avery, who first settled at Madagascar, to Capt. John Gow, and James Williams, his Lieutenant, Etc. who were hang'd at Execution Dock, June 11, 1725, for Piracy and Murther, and afterwards hang'd in Chains between Blackwall and Deptford"
+        if (selection == 'notoriousPirates'){
+            return <div className="box-shadow"> <LibraryOfCongressAPI referenceURL = 'resource/rbc0001.2019gen31667/?st=gallery'/> </div>
         }
+
+        // 1839 book:   "Lives and voyages of Drake, Cavendish, and Dampier"
+        if (selection == 'pirateLives'){
+            return <div className="box-shadow"> <LibraryOfCongressAPI referenceURL = 'resource/gdcmassbookdig.livesvoyagesofdr00john/?st=gallery&c=160'/> </div>
+        }
+
+        // 1856 book:   "The Pirates Own Book"
+        if (selection == 'piratesOwn'){
+            return <div className="box-shadow"> <LibraryOfCongressAPI referenceURL= 'resource/gdcmassbookdig.piratesownbook00ellm_0/?st=gallery' /> </div>
+        }
+
+        // 1874 book:   "Captain William Kidd and others of the pirates or buccaneers who ravaged the seas, the islands, and the continents of America two hundred years ago"
+        if (selection == 'ravagedSeas'){
+            return <div className="box-shadow"> <LibraryOfCongressAPI referenceURL= 'resource/gdcmassbookdig.captainwilliamki01abbo/?st=gallery' /> </div>
+        }
+
+        // 1910 thesis presented to the Board of Modern History of Oxford University: "The Buccaneers in the West Indies in the XVII century"
+        if (selection == 'buccaneersThesis'){
+            return <div className="box-shadow"> <LibraryOfCongressAPI referenceURL= 'resource/gdcmassbookdig.buccaneersinwest00hari/?st=gallery' /> </div>
+        }
+
     }
 
   
