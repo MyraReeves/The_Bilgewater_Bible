9a4d613051cc48457a866fbd4a3b29995e90dae9
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-02 21:41:26 -0800

    Deletes hasError from being a prop

--- JS320-final/src/App.jsx
+++ JS320-final/src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react';
 import './App.css';
 import RoutePaths from './RoutePaths';
 import Header from './Header'
-import Footer from './Footer'
+// import Footer from './Footer'
 
 
 function App() {
-  const [hasError, setHasError] = useState(false);
 
   return (
-    <body>
+    <>
       <Header />
-      <RoutePaths 
-        hasError={hasError} 
-        setHasError={setHasError}
-      />
-      <Footer />
+      <RoutePaths  />
+      {/* <Footer /> */}
       
-    </body>
+    </>
   )
 }
 
