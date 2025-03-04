54484b15d7d30c9c1d9b765f82cc52659c02124f
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-03 18:19:38 -0800

    Adds more book title options to dropdown menu

--- JS320-final/src/LearningDropdown.jsx
+++ JS320-final/src/LearningDropdown.jsx
@@ -14,14 +14,16 @@ function LearningDropdownMenu(props) {
   return (
     <div className="learning-dropdown">
         <form onSubmit={handleSubmit}>
-            <label htmlFor="learning-choices">Choose an option to read: </label><br/>
+            <label htmlFor="learning-choices">Choose which option you would like to read: </label><br/>
             <select name="learningLibrary" id="learning-choices" onChange={handleSelection} value={selection}>
                 <option disabled> </option>
                 <option value="bahamas">🌎 Learn more about the Bahamas</option>
                 <option value="notoriousPirates">📕1725 book - &#34;The History and Lives of All the Most Notorious Pirates and Their Crews&#34;</option>
-                <option value="pirateLives">📗1839 book - &#34;Lives and voyages of Drake, Cavendish, and Dampier&#34;</option>
-                <option value="piratesOwn">📙1856 book - &#34;The Pirates Own Book&#34;</option>
-                <option value="ravagedSeas">📘1874 book - &#34;Pirates or Buccaneers Who Ravaged the Seas [...] Two Hundred Years Ago&#34;</option>
+                <option value="pirateCaptains">📗1829 book - &#34;History of [...] Pirate Captains [...] Tried, Condemned, and Executed at Nassau on 10 of December 1718&#34;</option>
+                <option value="pirateLives">📙1839 book - &#34;Lives and voyages of Drake, Cavendish, and Dampier&#34;</option>
+                <option value="piratesOwn">📘1856 book - &#34;The Pirates Own Book&#34;</option>
+                <option value="ravagedSeas">📕1874 book - &#34;Pirates or Buccaneers Who Ravaged the Seas [...] Two Hundred Years Ago&#34;</option>
+                <option value="britishPrivateer">📗1894 book - &#34;Life Aboard a British Privateer in the Time of Queen Anne&#34;</option>
                 <option value="buccaneersThesis">📓1910 thesis, Oxford University - &#34;The Buccaneers in the West Indies in the XVII Century&#34;</option>
         </select>
         {/* <input type="submit" value="Submit"/> */}
