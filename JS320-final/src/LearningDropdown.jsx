5dfda086f1a6eefe7ca5936b6abbbdef8c632d7a
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-08 02:55:50 -0800

    Adds 1694 Spanish book to dropdown menu

--- JS320-final/src/LearningDropdown.jsx
+++ JS320-final/src/LearningDropdown.jsx
@@ -20,10 +20,11 @@ function LearningDropdownMenu(props) {
                 <option value="bahamas">🌎 Learn more about the Bahamas</option>
                 <option disabled>  </option>
                 <option value="spanishAccount">📔1681 book ✴️written in Spanish✴️ - &#34;Piratas de la America&#34; </option>
-                <option value="notoriousPirates">📕1725 book - &#34;The History and Lives of All the Most Notorious Pirates and Their Crews&#34;</option>
-                <option value="buccaneersAmerica">📗1816 book - &#34;History of the Buccaneers of America&#34;</option>
-                <option value="pirateCaptains">📙1829 book - &#34;History of [...] Pirate Captains [...] Tried, Condemned, and Executed at Nassau on 10 of December 1718&#34;</option>
-                <option value="pirateLives">📘1839 book - &#34;Lives and voyages of Drake, Cavendish, and Dampier&#34;</option>
+                <option value="spanishRecommendations">📕1694 book ✴️written in Spanish✴️ - &#34;Forma de Assegurar Los Puertos y Poblaciones de Todas las Costas de las Indias&#34;</option>
+                <option value="notoriousPirates">📗1725 book - &#34;The History and Lives of All the Most Notorious Pirates and Their Crews&#34;</option>
+                <option value="buccaneersAmerica">📙1816 book - &#34;History of the Buccaneers of America&#34;</option>
+                <option value="pirateCaptains">📘1829 book - &#34;History of [...] Pirate Captains [...] Tried, Condemned, and Executed at Nassau on 10 of December 1718&#34;</option>
+                <option value="pirateLives">📔1839 book - &#34;Lives and voyages of Drake, Cavendish, and Dampier&#34;</option>
                 <option value="piratesOwn">📕1856 book - &#34;The Pirates Own Book&#34;</option>
                 <option value="ravagedSeas">📗1874 book - &#34;Pirates or Buccaneers Who Ravaged the Seas [...] Two Hundred Years Ago&#34;</option>
                 <option value="britishPrivateer">📙1894 book - &#34;Life Aboard a British Privateer in the Time of Queen Anne&#34;</option>
