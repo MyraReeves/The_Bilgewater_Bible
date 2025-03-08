22767524a18acc266969085138e4148d5c0ae01e
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-07 19:03:02 -0800

    Displays character information on screen based on id prop passed into Firebase API

--- JS320-final/src/BlackSailsCharacters.jsx
+++ JS320-final/src/BlackSailsCharacters.jsx
@@ -1,31 +1,79 @@
-// This file uses the characters collection database inside my Firebase app to display information about the pirate characters onto the screen.
-
+// This file uses the "characters" collection database inside my Firebase app to display information about each requested character's information onto the screen (via an id prop that is passed in).
+import PropTypes from "prop-types"
+import { useEffect, useState } from "react"
 import database from './db';
-import { collection, getDocs } from "firebase/firestore";
-import { Link } from 'react-router-dom';
+import { doc, getDoc } from "firebase/firestore";
 
+function BlackSailsCharacters({characterID}) {
+  const [character, setCharacter] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
 
-function BlackSailsCharacters() {
+  useEffect( () => {
+    const getData = async () => {
+      try {
+        const docRef = doc(database, "pirate-info", characterID);
+        const docSnap = await getDoc(docRef);
 
+        if (docSnap.exists()) {
+          console.log("Document data:", docSnap.data().name);
+          setCharacter(docSnap.data())
+        } else {
+          console.log("No such document!");
+        }
+      }
+      catch (error) {
+          console.log('%cThe following error occured when attempting to fetch data from the API: \n', 'color: red; font-weight: bold; font-size: larger', error);
+          setHasError(true);
+        }
+      finally {
+        setLoading(false)
+      }
+    } 
+    getData()
+
+  }, [characterID])
+
+  console.log(character)
+
+  if (hasError) {
+    return <div className="error">⛔ An error occurred.  Sorry! ⛔ <br/>Please check the console for further details.</div>;
+  }
+  if (loading) {
+      return <div className="loading">Loading character information...</div>
+  }
 
   // Pirates should return slightly different, longer text than other characters.  If !pirate return
   // "{name} is a fictitious character created for the show. {Pronoun} is {height} {nationality} {gender} with {hair} hair and {eyes} eyes."
 
   return (
-    <main>
-      {/* 
-      <h1> {name} </h1>
-      <p> {name} is {position} of the pirate ship {shipName}, which is {shipDescription}. </p> 
-      <p> {firstName} is a {height} tall {nationality} {gender} with {hair} hair and {eyes} eyes from {city}. </p>
-      <p> {additionalInfo} </p>
+    <div>
 
-      <span className="underline">{realOrFiction}</span>:
-      <p> {inOtherSources[0]} </p>
-      <p> {inOtherSources[1]} </p>
-      <p> {inOtherSources[2]} </p>
-      */}
-    </main>
+      <div className="with-photo">
+        <img src={character.image} alt={character.altText}/>
+        <h1 className="underline">{character.name}</h1>
+        <p> 
+          {character.name} is a {character.height} tall {character.nationality} {character.gender} with {character.hair} hair and {character.eyes} eyes from {character.city}. 
+        </p>
+        <p>
+          {character.firstName} is {character.position} of the pirate ship {character.shipName}, which is {character.shipDescription}. 
+        </p>
+        <p> {character.additionalInfo} </p>
+      </div>
+      <div className="underneath-photo">
+        <h4><span className="underline">{character.realOrFiction}</span>:</h4>
+        <p className="indent"> {character.inOtherSources[0]} </p>
+        <p className="indent"> {character.inOtherSources[1]} </p>
+        <p className="indent"> {character.inOtherSources[2]} </p>
+      </div>
+
+    </div>
   )
+}
+
+
+BlackSailsCharacters.propTypes = {
+  characterID: PropTypes.string.isRequired
 }
 
 export default BlackSailsCharacters
