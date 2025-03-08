f24bacce359546ca065a8cd1d08831a8cdb21dcd
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-07 19:09:03 -0800

    Fixes reference path used for character info pages

--- JS320-final/src/RoutePaths.jsx
+++ JS320-final/src/RoutePaths.jsx
@@ -18,7 +18,7 @@ function RoutePaths() {
           <Route path='/stats' element={ <Stats/> } />
           <Route path='/cast' element={ <CastAPI/> } />
           <Route path='/characters' element={ <CharacterIndex/> } />
-          <Route path='/characters/:index' element={ <CharacterInfo/> } />
+          <Route path='/characters/:id' element={ <CharacterInfo/> } />
           <Route path='/podcast' element={ <Podcast/> } />
           <Route path='/learn' element= { <LearnMore/> } />
           <Route path='*' element={ <PageNotFound/> } />
