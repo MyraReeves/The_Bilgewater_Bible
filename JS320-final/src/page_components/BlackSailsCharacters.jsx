a87620514af9a5fa2d048e3492e5143a7c8f43b5
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-03-25 23:59:26 -0700

    Reduces wording in note

--- JS320-final/src/page_components/BlackSailsCharacters.jsx
+++ JS320-final/src/page_components/BlackSailsCharacters.jsx
@@ -1,4 +1,4 @@
-// This file uses the characters ("characters") collection database inside my Firebase app to display information about each requested character's information onto the screen (via an id prop that is passed in).
+// This file uses the "characters" collection database inside my Firebase app to display information about each requested character's information onto the screen (via an id prop that is passed in).
 import PropTypes from "prop-types"
 import { useEffect, useState } from "react"
 import database from '../db';
